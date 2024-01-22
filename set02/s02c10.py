14d740c618ccfc4bf7d555df2324e1dcbadac6e5
mza7a <mzhatimgames@gmail.com>
2024-01-22 15:41:40 +0100

    Updated s02c10

--- set02/s02c10.py
+++ set02/s02c10.py
@@ -13,14 +13,21 @@ def decrypt(ciphertext, key):
     plaintext = plain.decrypt(ciphertext)
     return plaintext
 
+def encrypt_cbc(key, iv, plaintext):
+    IV = iv
+    plainblocks = [plaintext[i:i+16] for i in range(0, len(plaintext), 16)]
+    cipherblocks = []
 
-def main():
-    KEY = b'YELLOW SUBMARINE'
-    IV = b'\x00'*16
-    is_first = 0
-    with open('s02c09_input', 'r') as f:
-        ciphertext = f.read()
-    ciphertext = base64.b64decode(ciphertext)
+    for block in plainblocks:
+        xored_block = xor(block, IV)
+        IV = encrypt(xored_block, key)
+        cipherblocks.append(IV)
+    ciphertext = b''.join(cipherblocks)
+    return ciphertext
+
+def decrypt_cbc(key, iv, ciphertext):
+    KEY = key
+    IV = iv
     cipherblocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
     plainblocks = []
     for block in cipherblocks:
@@ -28,8 +35,22 @@ def main():
         plainblocks.append(xor(dec_block, IV))
         IV = block
     plaintext = b''.join(plainblocks)
-    print(plaintext.decode())
+    return plaintext
 
+def main():
+    key = b'YELLOW SUBMARINE'
+    iv = b'\x00'*16
+    with open('s02c10_input', 'r') as f:
+        ciphertext = f.read()
+    ciphertext = base64.b64decode(ciphertext)
+    with open('s02c10_output', 'r') as e:
+        plaintext = e.read()
+    plaintext = bytes.fromhex(plaintext)
+    ciphertext2 = encrypt_cbc(key, iv, plaintext)
+    assert ciphertext == ciphertext2
+    plaintext2 = decrypt_cbc(key, iv, ciphertext)
+    assert plaintext == plaintext2
+    print("Both encryption and decryption working fine !!")
 
 if __name__ == "__main__":
     main()
